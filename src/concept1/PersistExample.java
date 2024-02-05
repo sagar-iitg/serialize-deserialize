88631d5624cab1ba5e65f2b7a2b0d7119b22bcbe
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-02-05 23:03:39 +0530

    Update PersistExample.java

--- src/concept1/PersistExample.java
+++ src/concept1/PersistExample.java
@@ -1,14 +1,14 @@
 package concept1;
 
-import java.io.FileOutputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 class PersistExample{
     public static void main(String args[])throws Exception{
         Student s1 =new Student(211,"ravi",22);//creating object
         //writing object into file
-        FileOutputStream f=new FileOutputStream("file.txt");
-        ObjectOutputStream out=new ObjectOutputStream(f);
+        FileOutputStream f=new FileOutputStream("file1.txt");
+        BufferedOutputStream x=new BufferedOutputStream(f);
+        ObjectOutputStream out=new ObjectOutputStream(x);
         out.writeObject(s1);
         out.flush();
         out.close();
