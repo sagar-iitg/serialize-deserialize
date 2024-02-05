0950ed3ba3b1412448b21602e1c0f7130a9d597d
Sagar Kumar <68909016+sagar-iitg@users.noreply.github.com>
2024-02-05 23:03:35 +0530

    Update DePersist.java

--- src/concept1/DePersist.java
+++ src/concept1/DePersist.java
@@ -4,8 +4,9 @@ import java.io.*;
 
 class DePersist{
     public static void main(String[] args)throws Exception{
-        FileInputStream f=new FileInputStream("file.txt");
-        ObjectInputStream in=new ObjectInputStream(f);
+        FileInputStream f=new FileInputStream("file1.txt");
+        BufferedInputStream bf=new BufferedInputStream(f);
+        ObjectInputStream in=new ObjectInputStream(bf);
         Student s=(Student)in.readObject();
         System.out.println(s.id+" "+s.name+" "+s.age);
         in.close();
